3aaacd7dfd6be90ebfa8c41f17f7b92087b409ca
Astxik <astxxajakyan@gmail.com>
2018-07-11 04:49:50 -0700

    Fix footer

--- src/App.js
+++ src/App.js
@@ -1,18 +1,41 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+import MenuItem from "./components/MenuItem";
+import menu from "./menu";
+import "./App.css";
 
 class App extends Component {
   render() {
+    const { classes, theme } = this.props;
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <Router>
+          <div>
+            <div className="main">
+              <div className="nav-bar">
+                {menu.map(item => {
+                  return (
+                    <MenuItem
+                      name={item.name}
+                      link={item.link}
+                      className="nav-item"
+                    />
+                  );
+                }, this)}
+              </div>
+            </div>
+
+            <main>
+              {menu.map(item => {
+                return (
+                  <Route exact path={item.link} component={item.component} />
+                );
+              })}
+            </main>
+          </div>
+        </Router>
       </div>
     );
   }
